db9f1576ec4f6f0f482b34ad7a1243637130a51a
Michael Salaverry <michaels@wix.com>
2017-08-08 18:47:10 +0300

    refactored tests for time format to use component drivers

--- src/LocaleClock.test.js
+++ src/LocaleClock.test.js
@@ -6,22 +6,32 @@ import moment from 'moment-timezone';
 
 import LocaleClock from './LocaleClock.js';
 
-it('renders without crashing', () => {
+const renderLocaleClock = (time=null) => {
     const div = document.createElement('div');
     ReactDOM.render(<LocaleClock key="1"
                                  id="1"
                                  tz="Asia/Omsk"
-                                 utc={moment()} />, div);
+                                 utc={time?moment(time):moment()} />, div);
+    return div;
+};
+
+const LocaleClockDriver = (DOMContainer) => ({
+    getTimeAsString: () => DOMContainer.querySelector('.time').innerHTML
 });
 
-it('shows time properly', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<LocaleClock key="1"
-                                 id="1"
-                                 tz="Asia/Omsk"
-                                 utc={moment()} />, div);
-    const time = div.querySelector('.time').innerHTML;
+it('renders without crashing', () => {
+    renderLocaleClock();
+});
+
+it('shows dynamic time properly', () => {
+    const time = LocaleClockDriver(renderLocaleClock()).getTimeAsString();
     console.log(time);
     expect(time).to.include(':');
     expect(time.length).to.be.above(7);
+    expect(time.split(':').length).to.equal(3);
+});
+
+it('shows static time properly', () => {
+    const time = LocaleClockDriver(renderLocaleClock(1318874398806)).getTimeAsString();
+    expect(time).to.eql('00:10:58');
 });
