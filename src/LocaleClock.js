2ac162824462a204ba868d191f24c77e45a467b4
Michael Salaverry <michaels@wix.com>
2017-08-08 12:27:07 +0300

    composed CSS and removed useless {Component} from what should be functional components

--- src/LocaleClock.js
+++ src/LocaleClock.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ButtonGroup from './ButtonGroup.js';
+import './LocaleClock.css';
 
 const LocaleClock = (props) => {
     return <section className="localeClock">
